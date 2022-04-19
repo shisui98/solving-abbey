684042642bc2d6c755d30529f79e3c6c4bbb8dc0
shisui98 <mitkar@tutanota.com>
2022-04-20 03:35:33 +0530

    Fixed Sum in loop, general solution, python 3.9.2

--- simple/suminloop.py
+++ simple/suminloop.py
@@ -1,2 +1,7 @@
 # Sum in loop, general solution, python 3.9.2
-print(sum(int(x) for x in input().split()))
+a, b = (input('start and end points for slicing: ').split());
+list1 = [int(x) for x in input('list input: ').split()];
+list2 = list1[int(a):int(b)];
+
+print(sum(list2));
+#print(sum(int(x) for x in input().split()))
