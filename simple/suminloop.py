a80a2d7a2831505540828d3d3738d064f626dfdb
shisui98 <mitkar@tutanota.com>
2022-04-20 03:41:34 +0530

    Cleaned up Sum in loop

--- simple/suminloop.py
+++ simple/suminloop.py
@@ -2,6 +2,5 @@
 a, b = (input('start and end points for slicing: ').split());
 list1 = [int(x) for x in input('list input: ').split()];
 list2 = list1[int(a):int(b)];
-
 print(sum(list2));
-#print(sum(int(x) for x in input().split()))
+
