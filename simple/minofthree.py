f604ac532ec7b8a735cf1c307ee8e34c9a7b5f0d
shisui98 <mitkar@tutanota.com>
2022-04-21 22:15:57 +0530

    added Minimum of three, python 3.9.2

---
+++ simple/minofthree.py
@@ -0,0 +1,20 @@
+N = input();
+for x in range(int(N)):
+    a, b, c = input().split()
+    print(min(int(a), int(b), int(c)), end=" ")
+
+# choosing minimum efficiently:
+# trick 1:
+# min=a;
+# if(min>b)
+#     min=b;
+# if(min>c)
+#     min=c;
+# // output min
+
+# trick 2:
+# if (a < b)
+#     b = a;
+# if (b < c)
+#     c = b;
+# // output c
