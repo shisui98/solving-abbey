4f6a66ee245497b5ea4624bd1c226955dd3e249f
shisui98 <mitkar@tutanota.com>
2022-04-22 19:20:41 +0530

    added rounding.py, python, 3.9.2

--- simple/rounding.py
+++ simple/rounding.py
@@ -1,1 +1,9 @@
 # Rounding, python 3.9.2
+from decimal import Decimal, ROUND_HALF_UP
+N = input();
+for i in range(int(N)):
+    a, b = input().split();
+    X = (int(a)/int(b));
+    X = (float("%.02f" % X));
+    X = Decimal(X).quantize(0, ROUND_HALF_UP)
+    print(X, end=" ");
