b362e2cf270e6481942bff9e1c0c4a1fc974342c
shisui98 <103405793+shisui98@users.noreply.github.com>
2022-07-03 08:17:51 +0530

    Delete sumsinloop.py

--- simple/sumsinloop.py
+++
@@ -1,6 +0,0 @@
-# Ben Schroeder, 2014, modified for python 3.9.2 by shisui98
-inte = int(input('number of pairs: '))
-
-for i in range(inte):
-    a, b = input('pairs: ').split()
-    print(int(a) + int(b))
